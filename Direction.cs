b65049b1dfa42b8a229d640e1d002881911c136c
Buyka196 <buykab196@gmail.com>
2017-05-01 03:15:45 +0900

    Add files via upload

---
+++ Direction.cs
@@ -0,0 +1,12 @@
+﻿namespace Breakout1.Enums
+{
+    public enum Directions//udirdlaga
+    {
+        Up,
+        UpAndLeft,
+        UpAndRight,
+        Down,
+        DownAndRight,
+        DownAndLeft
+    }
+}
